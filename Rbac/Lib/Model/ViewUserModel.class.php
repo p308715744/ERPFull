cae80dd41eac35ff3dccd1426b890f98ccbef392
neco <308715744@qq.com>
2014-06-12 09:48:29 +0800

    4

---
+++ Rbac/Lib/Model/ViewUserModel.class.php
@@ -0,0 +1,12 @@
+<?php
+
+class ViewUserModel extends RelationModel {
+	protected $trueTableName = 'myerpview_system_user';	
+	protected $pk = 'systemID';	
+	
+	protected $_link = array(
+	);
+	
+
+}
+?>
---
+++ rbac.php
@@ -0,0 +1,19 @@
+<?php
+define('IN_ET', TRUE);
+require('./define.inc.php');
+
+define('ET_ROOT', dirname(__FILE__));
+define('MODE_NAME','mycore');
+define('THINK_PATH',dirname(__FILE__).'/../ThinkPHP/');
+define('APP_NAME', 'Rbac');
+define('APP_PATH', './Rbac/');
+//define('DEFAULT_TYPE','default');
+//define('SITE_URL',ET_URL);
+define('APP_DEBUG', true);
+
+define('_PHP_FILE_',$_SERVER['SCRIPT_NAME']);//thinkphp 对php-cgi 解析bug
+
+require('./Myerp/Common/Function.php');
+require(THINK_PATH.'ThinkPHP.php');
+
+?>
